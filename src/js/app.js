063393fa3cd304750017c56d624ac136ac8e5c7a
Eliasmal Rodriguez <Eliasmal0228@gmail.com>
2025-03-31 08:50:34 +0000

    arreglar variable social media

--- src/js/app.js
+++ src/js/app.js
@@ -47,16 +47,15 @@ function render(variables = {}) {
     ? `<li><a href="https://instagram.com/${variables.instagram}"><i class="fab fa-instagram"></i></a></li>`
     : "";
 
-  let socialMediaPosition =
-    variables.socialMediaPosition === "" ? "position-left" : "position-right";
+  // let socialMediaPosition = variables.socialMediaPosition === "left" ? "position-left" : "position-right";
 
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
       ${cover}
       <img src="${variables.avatarURL}" class="photo" />
-      <h1>${fullName}</h1>
+      <h1>${fullName} </h1>
       <h2>${role}</h2>
       <h3>${location}</h3>
-      <ul class="${socialMediaPosition}">
+      <ul class="${variables.socialMediaPosition}">
         ${twitter}
         ${github}
         ${linkedin}
@@ -66,20 +65,20 @@ function render(variables = {}) {
   `;
 
   // reset the website body with the new html output
-  //   document.querySelector("#widget_content").innerHTML = `<div class="widget">
-  //             ${cover}
-  //           <img src="${variables.avatarURL}" class="photo" />
-  //           <h1>Lucy Boilett</h1>
-  //           <h2>Web Developer</h2>
-  //           <h3>Miami, USA</h3>
-  //           <ul class="position-right">
-  //             <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-  //             <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-  //             <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-  //             <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
-  //           </ul>
-  //         </div>
-  //     `;
+  // document.querySelector("#widget_content").innerHTML = `<div class="widget">
+  //           ${cover}
+  //         <img src="${variables.avatarURL}" class="photo" />
+  //         <h1>Lucy Boilett</h1>
+  //         <h2>Web Developer</h2>
+  //         <h3>Miami, USA</h3>
+  //         <ul class="position-right">
+  //           <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
+  //           <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
+  //           <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
+  //           <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+  //         </ul>
+  //       </div>
+  //   `;
 }
 
 /**
